6eef2ccfb348c4f9e9e6b2232a929a2b67e0942c
2501264abcc <2501264@s.asojuku.ac.jp>
2026-01-29 12:59:51 +0900

    Add files via upload

---
+++ 74/home_script.js
@@ -0,0 +1,10 @@
+function goRecord() 
+{
+  console.log("記録ページへ移動します");
+  location.href = "index.html";
+}
+function goHistory() 
+{
+  console.log("履歴ページへ移動します");
+  location.href = "movie.html";
+}
